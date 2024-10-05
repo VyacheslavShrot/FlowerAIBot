9fd8eb26165594e3ff4ec62f6187a597d8d509fc
crm.c.31904 <crm.c.31904@dz.com>
2024-10-05 20:57:25 +0200

    Added Module for Running Web

---
+++ run_web.py
@@ -0,0 +1,36 @@
+import os
+import subprocess
+import sys
+
+import uvicorn
+from fastapi import HTTPException
+
+
+async def _run_server():
+    # Get Config Path
+    config_dir_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'web/config'))
+
+    # Add To PATH Config Dir
+    sys.path.append(config_dir_path)
+
+    from web.config.logger import logger
+
+    try:
+        # Run pytest for Testing FastAPI APP Status and DB Users Status
+        subprocess.run(["pytest"])
+
+        logger.info("The application test has been successfully executed")
+    except Exception as e:
+        logger.error(f"An error occurred when running tests | {e}")
+        raise HTTPException(status_code=500, detail=f"An error occurred when running tests | {e}")
+
+    # Run FastAPI Application
+    uvicorn_cmd = "app:app"
+    uvicorn.run(uvicorn_cmd, host="0.0.0.0", port=8080, reload=True)
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    # Run Async Module
+    asyncio.run(_run_server())
